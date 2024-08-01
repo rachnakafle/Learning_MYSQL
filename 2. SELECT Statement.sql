9a7ed57de8f26c1477c11cd98e80d1132677950e
Rachna Kafle <rachanakafle99@gmail.com>
2024-08-01 15:29:05 -0400

    Learnt SELECT on Day 2

--- 2. SELECT Statement.sql
+++ 2. SELECT Statement.sql
@@ -13,3 +13,36 @@ FROM parks_and_recreation.employee_demographics;
 SELECT DISTINCT first_name, gender
 FROM parks_and_recreation.employee_demographics;
 
+SELECT * 
+FROM employee_salary
+WHERE first_name = 'Leslie';
+
+SELECT * 
+FROM employee_demographics
+WHERE gender != 'FEMALE';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01';
+
+# --AND OR NOT --Logical Operators
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR NOT gender = 'male';
+
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) OR age > 55;
+
+-- LIKE STATEMENT
+-- % AND _
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___%';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date LIKE '1989%';
