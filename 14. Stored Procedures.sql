7527a67cd6309bfe8122f71a1598f67676ed3b93
Rachna Kafle <rachanakafle99@gmail.com>
2024-08-06 00:50:31 -0400

    Learnt About Stored Procedures

---
+++ 14. Stored Procedures.sql
@@ -0,0 +1,32 @@
+-- STORED PROCEDURES
+
+SELECT * 
+FROM employee_salary
+WHERE salary >= 50000;
+
+DELIMITER $$ 
+CREATE PROCEDURE large_salaries3()
+BEGIN
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 50000;
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 10000;
+END $$
+DELIMITER ;
+
+CALL large_salaries3()
+
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries4 (p_employee_id INT)
+BEGIN
+	SELECT salary
+    FROM employee_salary
+    WHERE employee_id = huggymuffin
+    ;
+END $$
+DELIMITER ;
+
+CALL large_salaries4(1);
