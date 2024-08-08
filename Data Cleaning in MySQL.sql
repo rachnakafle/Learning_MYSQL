aeb6be248d1f8e1bb047de8e3395595edf5310e2
Rachna Kafle <rachanakafle99@gmail.com>
2024-08-08 03:59:45 -0400

    Learnt about Duplicate CTE

--- Data Cleaning in MySQL.sql
+++ Data Cleaning in MySQL.sql
@@ -1,1 +1,37 @@
 -- DATA CLEANING
+
+SELECT * 
+FROM layoffs;
+
+-- 1. Remove Duplicates
+-- 2. Standardized the data
+-- 3. Null Values or Blank Values
+-- 4. Remove Any Columns
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+SELECT *,
+ROW_NUMBER() OVER( 
+PARTITION BY Company, Industry, Total_laid_off, Percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging;
+
+WITH duplicate_cte AS 
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Casper';
+-- INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+
