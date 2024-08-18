f88377217be090ed71135b4d266756cc0606265b
Rachna Kafle <rachanakafle99@gmail.com>
2024-08-18 17:29:27 -0400

    Working on deleting null values

--- Data Cleaning 2.sql
+++ Data Cleaning 2.sql
@@ -1,6 +1,6 @@
 -- DATA CLEANING
 
-SELECT * 
+SELECT *
 FROM layoffs;
 
 -- 1. Remove Duplicates
@@ -8,38 +8,37 @@ FROM layoffs;
 -- 3. Null Values or Blank Values
 -- 4. Remove Any Columns
 
-CREATE TABLE layoffs_staging
+CREATE TABLE layoffs_staging3
 LIKE layoffs;
 
-SELECT *,
-ROW_NUMBER() OVER( 
-PARTITION BY Company, Industry, Total_laid_off, Percentage_laid_off, `date`) AS row_num
-FROM layoffs_staging;
+SELECT *
+FROM layoffs_staging3;
 
-WITH duplicate_cte AS 
+INSERT layoffs_staging3
+SELECT *
+FROM layoffs;
+
+SELECT *,
+ROW_NUMBER() OVER(
+	PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging3;
+
+WITH duplicate_cte AS
 (
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
-FROM layoffs_staging
+FROM layoffs_staging3
 )
-
 DELETE 
-FROM duplicate_cte
-WHERE row_num > 1
-SELECT *
 FROM duplicate_cte
 WHERE row_num > 1;
 
 SELECT *
-FROM layoffs_staging
+FROM layoffs_staging3
 WHERE company = 'Casper';
--- INSERT layoffs_staging
-SELECT *
-FROM layoffs;
 
-
-CREATE TABLE `layoffs_staging2` (
+CREATE TABLE `layoffs_staging4` (
   `company` text,
   `location` text,
   `industry` text,
@@ -48,15 +47,24 @@ CREATE TABLE `layoffs_staging2` (
   `date` text,
   `stage` text,
   `country` text,
-  `funds_raised_millions` int DEFAULT NULL
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+SELECT * 
+FROM layoffs_staging4
+WHERE row_num > 1;
 
-SELECT *
-FROM layoffs_staging2;
-
-INSERT INTO layoffs_staging2
+INSERT INTO layoffs_staging4
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
-FROM layoffs_staging;
+FROM layoffs_staging3;
+
+DELETE  
+FROM layoffs_staging4
+WHERE row_num > 1;
+
+SELECT * 
+FROM layoffs_staging4
+WHERE row_num > 1;
