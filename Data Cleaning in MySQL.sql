24f2f4c6f22f92da42cf126dfa38fd14af379ce6
Rachna Kafle <rachanakafle99@gmail.com>
2024-10-01 01:37:30 -0400

    Deleted row num > 1

--- Data Cleaning in MySQL.sql
+++ Data Cleaning in MySQL.sql
@@ -64,7 +64,21 @@ CREATE TABLE `layoffs_staging2` (
 SELECT *
 FROM layoffs_staging2;
 
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
 
+DELETE 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+-- STANDARDIZING DATA
 
 
 
