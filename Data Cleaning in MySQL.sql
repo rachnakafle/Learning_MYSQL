51a0e0a7411b77a9c659a16e44b50adde55c80eb
Rachna Kafle <rachanakafle99@gmail.com>
2024-10-01 03:21:33 -0400

    Standardizing the data

--- Data Cleaning in MySQL.sql
+++ Data Cleaning in MySQL.sql
@@ -80,5 +80,103 @@ WHERE row_num > 1;
 
 -- STANDARDIZING DATA
 
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
 
+UPDATE layoff_staging2
+SET company = TRIM(company);
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT industry
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+SELECT `date`,
+STR_TO_DATE(`date`, '%m/%d/%Y ')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y ');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+--------- FROM CHAT GPT---------
+
+CREATE TABLE temp_table AS
+SELECT company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, funds_raised_millions
+FROM layoffs_staging2
+GROUP BY company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, funds_raised_millions;
+
+DROP TABLE layoffs_staging2;
+
+RENAME TABLE temp_table TO layoffs_staging2;
+
+--------
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging t1
+JOIN layoffs_staging t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
 
